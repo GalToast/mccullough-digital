15662322fede91ac8b6d9914fbb9e72bfc86319d
McCullough Digital <FredJaur33guy@hotmail.com>
2025-09-27 01:19:04 -0500

    Add InnerBlocks configuration to services block

--- blocks/services/editor.js
+++ blocks/services/editor.js
@@ -1,8 +1,21 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { useBlockProps, RichText, InnerBlocks } from '@wordpress/block-editor';
 
 import metadata from './block.json';
+
+const {
+    innerBlocks: {
+        allowedBlocks: allowedServiceBlocks = [
+            'mccullough-digital/service-card',
+        ],
+        template: servicesTemplate = [
+            ['mccullough-digital/service-card'],
+            ['mccullough-digital/service-card'],
+            ['mccullough-digital/service-card'],
+        ],
+    } = {},
+} = metadata;
 
 registerBlockType(metadata.name, {
     ...metadata,
@@ -11,12 +24,16 @@ registerBlockType(metadata.name, {
         const blockProps = useBlockProps();
 
         return (
-            <section { ...blockProps }>
+            <section {...blockProps}>
                 <RichText
                     tagName="h2"
                     value={ headline }
                     onChange={ (value) => setAttributes({ headline: value }) }
                     placeholder={ __('Add services headline…', 'mccullough-digital') }
+                />
+                <InnerBlocks
+                    allowedBlocks={ allowedServiceBlocks }
+                    template={ servicesTemplate }
                 />
             </section>
         );
