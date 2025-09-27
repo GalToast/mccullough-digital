3da80b250dd5161ca7a56fa9bad85e2f528243f2
McCullough Digital <FredJaur33guy@hotmail.com>
2025-09-27 14:25:23 -0500

    Add post meta pattern and block comments to singular template

---
+++ patterns/post-meta.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: Post Meta
+ * Slug: mccullough-digital/post-meta
+ * Categories: mccullough-digital-sections
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"className":"entry-footer","layout":{"type":"flex","orientation":"vertical","justifyContent":"flex-start","flexWrap":"wrap"}} -->
+<div class="wp-block-group entry-footer">
+  <!-- wp:group {"className":"entry-meta","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"flex-start"}} -->
+  <div class="wp-block-group entry-meta">
+    <!-- wp:post-date {"format":"F j, Y"} /-->
+    <!-- wp:post-terms {"term":"category","prefix":" • ","separator":", "} /-->
+  </div>
+  <!-- /wp:group -->
+  <!-- wp:post-terms {"term":"post_tag","className":"tag-links","separator":", "} /-->
+</div>
+<!-- /wp:group -->
